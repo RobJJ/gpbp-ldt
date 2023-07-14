560278a832fddb14fa2718f3c5153f4530d4e892
RobJ <robjjohnsenwork@gmail.com>
2023-07-14 10:33:06 +0700

    updated page right hand comp child

--- src/app/dashboard/[country]/page.js
+++ src/app/dashboard/[country]/page.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function CountryPage() {
-  return <div className="w-1/2 h-full bg-purple-300">CountryPage</div>;
+  return (
+    <div className="w-1/2 h-full bg-purple-300 flex flex-col p-2 gap-2">
+      <section className="w-full h-1/2 bg-purple-100">
+        Top: Facts cards for country level shit. We need country data from DB
+        here.. the data we fetch is just based on what country we are looking
+        at.. ezzee
+      </section>
+      <section className="w-full h-1/2 bg-purple-100">
+        Bottom: A list of provinces for country level shit. We need the province
+        data here. the data we fetch is just based on the country and then we
+        pass in [country]-province-data
+      </section>
+    </div>
+  );
 }
--- src/components/clientcompmap.js
+++ src/components/clientcompmap.js
@@ -12,7 +12,8 @@ export default function ClientCompMap({ chosenCountry }) {
     router.push(`/dashboard/${chosenCountry}?layer=district`);
   }
   return (
-    <div className="w-1/2 h-full bg-purple-200">
+    <div className="w-1/2 h-full bg-purple-200 flex flex-col gap-2">
+      <span>This visual component is fixed at [country] level</span>
       <button onClick={handleClick} className="bg-slate-200 p-1">
         Change the router
       </button>
