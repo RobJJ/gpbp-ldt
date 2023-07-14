d7e115918e24ba8ccb0ace2f5ec4f5dd9a82d418
RobJ <robjjohnsenwork@gmail.com>
2023-07-14 18:21:58 +0700

    found way to overwrite the searchParam within filter button

---
+++ src/components/FilterBarButtonYear.jsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import { FaAngleDown } from "react-icons/fa";
+
+const availableYears = [
+  { id: 1, year: "2022" },
+  { id: 2, year: "2021" },
+  { id: 3, year: "2020" },
+  { id: 4, year: "2019" },
+];
+
+export default function FilterBarButtonYear({ paramYear, createQueryString }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const [year, setYear] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleCountrySelect = (e) => {
+    const choice = e.target.dataset.tag;
+
+    setYear(choice);
+    router.push(pathname + "?" + createQueryString("year", choice));
+    // createQueryString("year", choice);
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className=" flex gap-2 ">
+      <div>
+        <span className="rounded-md shadow-sm">
+          <button
+            onClick={toggleDropdown}
+            type="button"
+            className="inline-flex justify-center items-center w-28 h-5 rounded-md border border-gray-400 px-4 py-1 bg-white text-md leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
+          >
+            {year ? year : paramYear}
+            <FaAngleDown className="text-base ml-1" />
+          </button>
+        </span>
+      </div>
+      {isOpen && (
+        <div className=" absolute mt-12 w-32 h-10 rounded-md shadow-lg">
+          <div className="rounded-md bg-white shadow-xs">
+            <div
+              className="py-1"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="options-menu"
+            >
+              {availableYears.map((year, idx) => {
+                return (
+                  <span
+                    onClick={handleCountrySelect}
+                    data-tag={year.year}
+                    className="font-bold text-center block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out cursor-pointer"
+                    role="menuitem"
+                    key={year.id}
+                  >
+                    {year.year}
+                  </span>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,15 +1,32 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import FilterBarButtonYear from "./FilterBarButtonYear";
+import { useCallback } from "react";
 
 // start as server component -> not sure if needs to rather be client... lets see
 // this buttons purpose is the change the search params
 
 export default function FilterButton({}) {
   const searchParams = useSearchParams();
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
 
   return (
     <div className="w-full bg-blue-300 flex justify-center p-2 gap-5">
+      <FilterBarButtonYear
+        paramYear={searchParams.get("year")}
+        createQueryString={createQueryString}
+      />
       <span>year: {searchParams.get("year")}</span>
       <span>score_one: {searchParams.get("score_one")}</span>
       <span>score_two: {searchParams.get("score_two")}</span>
