ed63b8de7f521e2c252e2b9dc273c2830838b072
RobJ <robjjohnsenwork@gmail.com>
2023-07-21 12:39:01 +0700

    added two comps for map-scatter toggle visual

--- src/app/atom/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { exampleNumberAtom } from "@/lib/atoms";
-import { useAtom } from "jotai";
-
-export default function AtomPage() {
-  const [number, setNumber] = useAtom(exampleNumberAtom);
-  return <div>Dis page brah,, numbaah : {number}</div>;
-}
--- src/components/clientcompvisual.js
+++ src/components/clientcompvisual.js
@@ -1,5 +1,7 @@
 "use client";
 
+import { visualTypeSelected } from "@/lib/atoms";
+import { useAtom } from "jotai";
 import {
   usePathname,
   useSearchParams,
@@ -18,49 +20,25 @@ export default function ClientCompVisual() {
   //
   const params = useParams();
   const searchParams = useSearchParams();
-  console.log("[ClientCompVisual] : rendered : params:", params);
+  const [visualType, setVisualType] = useAtom(visualTypeSelected);
+  console.log("[ClientCompVisual] : rendered");
   // console.log("[ClientCompVisual] : rendered : searchParams:", searchParams);
 
   // function handleClick() {
   //   router.push(`/dashboard/${chosenCountry}?layer=district`);
   // }
   return (
-    <div className="w-full h-full bg-purple-200 flex flex-col gap-2">
-      <span>
-        This visual component is fixed at [country] level. Im getting data based
-        on the params.....
-      </span>
-      <div>
-        {params.country && <span>{params.country}</span>}
-        {params.province && <span>{params.province}</span>}
-        {params.district && <span>{params.district}</span>}
-        {/*<button onClick={handleClick} className="bg-slate-200 p-1">
-        Change the router
-  </button>*/}
-      </div>
-      <div className="flex flex-col">
-        <span>year:{searchParams.get("year")}</span>
-        <span>score_one: {searchParams.get("score_one")}</span>
-        <span>score_two: {searchParams.get("score_two")}</span>
-      </div>
-      {/* these two components can change and mutate the params and searchParams: NB! */}
-      {searchParams.get("score_one") === "air" && (
-        <div className="flex flex-col">
-          <span>MAP VISUAL RENDERED</span>
-          <span>
-            we will render the map here if searchParam is correct: just testing
-            with score_one..
-          </span>
-        </div>
+    <div className="w-full h-full flex flex-col gap-2">
+      {visualType === "scatter" && (
+        <section className="w-full h-full bg-yellow-200">
+          I AM SCATTER COMPONENT
+        </section>
       )}
-      {searchParams.get("score_one") === "forest" && (
-        <div className="flex flex-col">
-          <span>Scatter VISUAL RENDERED</span>
-          <span>
-            we will render the Scatter here if searchParam is correct: just
-            testing with score_one
-          </span>
-        </div>
+      {visualType === "map" && (
+        <section className="w-full h-full bg-yellow-300">
+          {" "}
+          I AM MAP COMPONENT
+        </section>
       )}
     </div>
   );
