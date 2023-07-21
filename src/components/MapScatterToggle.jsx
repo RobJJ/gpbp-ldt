3b2d60130a877756da87d7a51d53f23fbc7053ed
RobJ <robjjohnsenwork@gmail.com>
2023-07-21 11:26:41 +0700

    added state for visualType atom to handle map or scatter

--- src/components/MapScatterToggle.jsx
+++ src/components/MapScatterToggle.jsx
@@ -1,10 +1,15 @@
 "use client";
 import React, { useState } from "react";
 import { animated, useSpring, config } from "react-spring";
+import { visualTypeSelected } from "@/lib/atoms";
+import { useAtom } from "jotai";
 
 function SwitchComponent() {
+  // default is false which sits on "Map" or right hand side
   const [visual, setVisual] = useState(false);
-
+  // visual atom: defaults to "map",, can be "map" or "scatter"
+  const [visualType, setVisualType] = useAtom(visualTypeSelected);
+  console.log("[SwitchComponent] : visualType :", visualType);
   const props = useSpring({
     left: visual ? "0%" : "50%",
     backgroundColor: "#4345AA",
@@ -14,6 +19,12 @@ function SwitchComponent() {
   const scatterButtonStyle = visual ? "text-white" : "text-black";
   const mapButtonStyle = visual ? "text-black" : "text-white";
 
+  const handleToggle = () => {
+    setVisual(!visual);
+    if (visual) setVisualType("map");
+    if (!visual) setVisualType("scatter");
+  };
+
   return (
     <div className="relative w-32 h-8 bg-white rounded-full">
       <animated.div className="absolute h-full rounded-full" style={props}>
@@ -22,13 +33,13 @@ function SwitchComponent() {
       <div className="absolute inset-0 flex items-center justify-between">
         <button
           className={`w-32 h-full flex items-center justify-center rounded-full bg-transparent focus:outline-none ${scatterButtonStyle}`}
-          onClick={() => setVisual(true)}
+          onClick={handleToggle}
         >
           {"Scatter"}
         </button>
         <button
           className={`w-32 h-full flex items-center justify-center rounded-full bg-transparent  focus:outline-none ${mapButtonStyle}`}
-          onClick={() => setVisual(false)}
+          onClick={handleToggle}
         >
           {"Map"}
         </button>
--- src/lib/atoms.js
+++ src/lib/atoms.js
@@ -1,3 +1,9 @@
 import { atom } from "jotai";
 
 export const exampleNumberAtom = atom(5);
+
+// ****** testing atom ******
+// this atom will handle the state of the visual component type, ie: scatter / Map
+// values can be "map" or "scatter"
+export const visualTypeSelected = atom("map");
+// export
