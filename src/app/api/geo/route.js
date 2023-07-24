1e8d082cdd5a408f8b1c17b5e1d27cd93f29b7da
RobJ <robjjohnsenwork@gmail.com>
2023-07-24 15:57:02 +0700

    added new route test for geo

---
+++ src/app/api/geo/route.js
@@ -0,0 +1,20 @@
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+// lets try get all districts at the api/route
+
+// lets try grab all geo first
+// then lets try grab only geodata that has a province_id of whats being passed in
+export async function GET(request, { params }) {
+  console.log(
+    "are you hitting this endpoint on change of params or searchparams"
+  );
+  const client = await clientPromise;
+  const db = client.db(process.env.MONGO_DB_NAME);
+  const data = await db
+    .collection(`kosovo-district-geojson`)
+    .find({}, { _id: 0 })
+    .toArray();
+  // allDistricts in an array of objects
+  return NextResponse.json(data);
+}
