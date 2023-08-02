21bda58741ac15936b8b880b7b6fcb664f82655b
RobJ <robjjohnsenwork@gmail.com>
2023-08-02 11:32:46 +0700

    added dynamic tab changing handled by parent

--- src/components/ProvincePageVisualChild-Visual.jsx
+++ src/components/ProvincePageVisualChild-Visual.jsx
@@ -1,7 +1,11 @@
 "use client";
 
-export default function ProvincePageVisualChildVisual() {
+export default function ProvincePageVisualChildVisual({ selectedTab }) {
+  // listen for the change of selectedTab value and filter the data based on what that tab needs
+
   return (
-    <section className="w-full h-full bg-blue-300">Visual section</section>
+    <section className="w-full h-full bg-blue-300">
+      Visual section: {selectedTab}
+    </section>
   );
 }
--- src/components/ProvincePageVisualParent.jsx
+++ src/components/ProvincePageVisualParent.jsx
@@ -8,13 +8,15 @@ import ProvincePageVisualChildVisual from "./ProvincePageVisualChild-Visual";
 // it will handle the state to sync the tabs and the lineChart
 export default function ProvincePageVisualParent({ provinceData }) {
   const [selectedTab, setSelectedTab] = useState("Overview");
+  console.log("[PARENT] : tab is:", selectedTab);
   return (
     <div className="w-full h-full flex flex-col">
       <ProvincePageVisualChildHeader
         selectedTab={selectedTab}
         setSelectedTab={setSelectedTab}
       />
-      <ProvincePageVisualChildVisual />
+      {/* the scatter component that will render content based on selectedTab */}
+      <ProvincePageVisualChildVisual selectedTab={selectedTab} />
     </div>
   );
 }
