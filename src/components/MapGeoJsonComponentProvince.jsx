7e056324d4959398c0b6adbbf370fa39ffd7bfb9
RobJ <robjjohnsenwork@gmail.com>
2023-08-09 22:17:43 +0700

    commented on process - added click event back

--- src/components/MapGeoJsonComponentProvince.jsx
+++ src/components/MapGeoJsonComponentProvince.jsx
@@ -35,24 +35,18 @@ export default function MapGeoJsonComponentProvince({
     if (!provinceSelected) setCurrentGeoLayers(provinceGeoData);
     //
     if (provinceSelected) {
+      // 1) create new copy of province data
       const newData = JSON.parse(JSON.stringify(provinceGeoData));
-      // remove the selected province feature from the array to add to back
 
+      // 2) store id of selected province
       const province_id = getProvinceId(gedDataProvince, provinceSelected);
-
+      // 3) find index of selected province in array and remove it from the copy while storing it
       const indexOfProvince = newData[0].features.findIndex(
         (feature) => feature.properties.GID_1 === province_id
       );
       const provinceFeature = newData[0].features.splice(indexOfProvince, 1);
-      // const newDataWithoutSelectedProvince = newData[0].features.filter(
-      //   (feature) => {
-      //     if (feature.properties.GID_1 !== province_id) return feature;
-      //   }
-      // );
-      console.log("my newDAta at end ::", newData);
-      console.log("provinceFeature ::", provinceFeature);
       //
-      // console.log("province_id ::", province_id);
+      // 4) collect all districts that belong in selected province
       const geoDistrictsInSelectedProvince = districtGeoData[0].features.filter(
         (feature) => {
           if (feature.properties.GID_1 === province_id) {
@@ -61,7 +55,8 @@ export default function MapGeoJsonComponentProvince({
         }
       );
 
-      // create a new object that contains all props of default but under features, you spread the new district layers in.
+      // 5) create new object that contains all province data + districts in province
+      // spread the features in a unique way to help with below mentioned
       // ** NB **
       // the order here is very important. Let province layer be just before the selected districts
       // this helps organise the layers for clicks and style them correctly
@@ -73,13 +68,11 @@ export default function MapGeoJsonComponentProvince({
           ...geoDistrictsInSelectedProvince,
         ],
       };
-      // console.log("updatedNewData :: ", updatedNewData);
+      // 6) set State for GEOJSON component
 
       setCurrentGeoLayers(updatedNewData);
       return;
     }
-    // if there is a province selected:: first spread default into an array, then continue with below
-    //  1) find province_id 2) filter matched districts that belong to this province_id 3) spread these districts into the geoData array
   }, [provinceSelected]);
   //
   const style = (feature) => {
@@ -120,58 +113,25 @@ export default function MapGeoJsonComponentProvince({
         fillColor: "#DFDFDF",
       };
     }
-
-    // console.log("feature from style is : ", feature);
-    // if provinceSelected matches the feature.properties.NAME_1 then you must apply unique stlying
-    // if (
-    //   decodeURIComponent(provinceSelected) === feature.properties.NAME_1 &&
-    //   !districtSelected
-    // ) {
-    //   // console.log(
-    //   //   "feature made it pass,, lets see what districts look like::",
-    //   //   feature
-    //   // );
-    //   // check if feature is a
-
-    //   return {
-    //     dashArray: "0",
-    //     color: "#F00",
-    //     weight: 4,
-    //     opacity: 1,
-    //     // ++ this is where layer score fill will come in with searchParams
-    //     fillOpacity: 0.7,
-    //     fillColor: "#DFDFDF",
-    //   };
-    // } else {
-    //   return {
-    //     dashArray: "0",
-    //     color: "#666",
-    //     weight: 1,
-    //     opacity: 0.5,
-    //     // ++ this is where layer score fill will come in with searchParams
-    //     fillOpacity: 0.7,
-    //     fillColor: "#DFDFDF",
-    //   };
-    // }
   };
   const handleLayerClick = (e) => {
     // GID_1 is province ID
     // GID_2 is district ID
     console.log("***handleLayerClick*** : event :", e);
-    // const country = e.target.feature.properties.COUNTRY.toLowerCase();
-    // const province = e.target.feature.properties.NAME_1;
-    // const district = e.target.feature.properties.NAME_2;
+    const country = e.target.feature.properties.COUNTRY.toLowerCase();
+    const province = e.target.feature.properties.NAME_1;
+    const district = e.target.feature.properties.NAME_2;
     // //
-    // const year = searchParams.get("year");
-    // const score_one = searchParams.get("score_one");
-    // const score_two = searchParams.get("score_two");
-    // //
-    // //
+    const year = searchParams.get("year");
+    const score_one = searchParams.get("score_one");
+    const score_two = searchParams.get("score_two");
+
     // // GID_2 is not true means this layerClick is a province
-    // if (!e.target.feature.properties.GID_2) {
-    //   router.push(
-    //     `/dashboard/${country}/${province}?year=${year}&score_one=${score_one}&score_two=${score_two}`
-    //   );
+    if (!e.target.feature.properties.GID_2) {
+      router.push(
+        `/dashboard/${country}/${province}?year=${year}&score_one=${score_one}&score_two=${score_two}`
+      );
+    }
     // } else {
     //   router.push(
     //     `/dashboard/${country}/${province}/${district}?year=${year}&score_one=${score_one}&score_two=${score_two}`
