7dbc8afe2a503e056a1bb0645583974acdab5e0c
RobJ <robjjohnsenwork@gmail.com>
2023-08-10 12:15:49 +0700

    district map version - added click event for district - stlying applied

--- src/components/MapGeoJsonComponentDistrict.jsx
+++ src/components/MapGeoJsonComponentDistrict.jsx
@@ -22,45 +22,59 @@ export default function MapGeoJsonComponentDistrict({
   //
   //
   const style = (feature) => {
-    // console.log("feature from style is : ", feature);
-    // if provinceSelected matches the feature.properties.NAME_1 then you must apply unique stlying
+    // 1) District selected. Feature matches selection
     if (
-      provinceSelected === feature.properties.NAME_1 &&
-      !feature.properties.NAME_2
+      provinceSelected &&
+      districtSelected &&
+      decodeURIComponent(districtSelected) === feature.properties.NAME_2
     ) {
       return {
         // need add a color matching function and pass in the score of the feature E8E8E8
-        fillColor: "#DFDFDF",
+        dashArray: "1",
+        color: "#F00",
         weight: 4,
         opacity: 1,
-        color: "#F00",
-        dashArray: "1",
-        fillOpacity: 0.7,
-      };
-    } else {
-      return {
-        // need add a color matching function and pass in the score of the feature E8E8E8
         fillColor: "#DFDFDF",
-        weight: 1,
-        opacity: 1,
-        color: "#666",
-        dashArray: "3",
         fillOpacity: 0.7,
       };
     }
+    // console.log("feature from style is : ", feature);
+    // if provinceSelected matches the feature.properties.NAME_1 then you must apply unique stlying
+    // if (
+    //   provinceSelected === feature.properties.NAME_1 &&
+    //   !feature.properties.NAME_2
+    // ) {
+
+    // } else {
+    return {
+      // need add a color matching function and pass in the score of the feature E8E8E8
+      fillColor: "#DFDFDF",
+      weight: 1,
+      opacity: 1,
+      color: "#666",
+      dashArray: "3",
+      fillOpacity: 0.7,
+    };
+    // }
   };
   const handleLayerClick = (e) => {
     // GID_1 is province ID
     // GID_2 is district ID
     console.log("***handleLayerClick*** : event :", e);
-    // const country = e.target.feature.properties.COUNTRY.toLowerCase();
-    // const province = e.target.feature.properties.NAME_1;
-    // const district = e.target.feature.properties.NAME_2;
+    const country = e.target.feature.properties.COUNTRY.toLowerCase();
+    const province = e.target.feature.properties.NAME_1;
+    const district = e.target.feature.properties.NAME_2;
     // //
-    // const year = searchParams.get("year");
-    // const score_one = searchParams.get("score_one");
-    // const score_two = searchParams.get("score_two");
-    // //
+    const year = searchParams.get("year");
+    const score_one = searchParams.get("score_one");
+    const score_two = searchParams.get("score_two");
+    //
+    // 1) User clicks a district
+    if (e.target.feature.properties.GID_2) {
+      router.push(
+        `/dashboard/${country}/${province}/${district}?year=${year}&score_one=${score_one}&score_two=${score_two}`
+      );
+    }
     // //
     // // GID_2 is not true means this layerClick is a province
     // if (!e.target.feature.properties.GID_2) {
@@ -68,9 +82,7 @@ export default function MapGeoJsonComponentDistrict({
     //     `/dashboard/${country}/${province}?year=${year}&score_one=${score_one}&score_two=${score_two}`
     //   );
     // } else {
-    //   router.push(
-    //     `/dashboard/${country}/${province}/${district}?year=${year}&score_one=${score_one}&score_two=${score_two}`
-    //   );
+
     // }
   };
   function onEachFeature(feature, layer) {
