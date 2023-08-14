f54c021bd594eca30f8eaa920dedc7bbecd9bc02
RobJ <robjjohnsenwork@gmail.com>
2023-08-14 17:54:37 +0700

    reworked scatter/map toggle and reduced complexity

--- src/components/MapScatterToggle.jsx
+++ src/components/MapScatterToggle.jsx
@@ -5,24 +5,28 @@ import { visualTypeSelected } from "@/lib/atoms";
 import { useAtom } from "jotai";
 
 function SwitchComponent() {
-  // default is false which sits on "Map" or right hand side
-  const [visual, setVisual] = useState(false);
   // visual atom: defaults to "map",, can be "map" or "scatter"
   const [visualType, setVisualType] = useAtom(visualTypeSelected);
   // console.log("[SwitchComponent] : visualType :", visualType);
   const props = useSpring({
-    left: visual ? "0%" : "50%",
+    left: visualType === "scatter" ? "0%" : "50%",
     backgroundColor: "#4345AA",
     config: { tension: 280, friction: 25 }, // increased tension and reduced friction
   });
 
-  const scatterButtonStyle = visual ? "text-white" : "text-black";
-  const mapButtonStyle = visual ? "text-black" : "text-white";
+  const scatterButtonStyle =
+    visualType === "scatter" ? "text-white" : "text-black";
+  const mapButtonStyle = visualType === "scatter" ? "text-black" : "text-white";
 
-  const handleToggle = () => {
-    setVisual(!visual);
-    if (visual) setVisualType("map");
-    if (!visual) setVisualType("scatter");
+  const handleToggle = (e) => {
+    const choice = e.target.getAttribute("data-tag");
+    if (visualType === "scatter" && choice === "map") {
+      setVisualType(choice);
+    }
+    if (visualType === "map" && choice === "scatter") {
+      setVisualType(choice);
+    }
+    return;
   };
 
   return (
@@ -32,12 +36,14 @@ function SwitchComponent() {
       </animated.div>
       <div className="absolute inset-0 flex items-center justify-between">
         <button
+          data-tag="scatter"
           className={`w-32 h-full flex items-center justify-center rounded-full bg-transparent focus:outline-none ${scatterButtonStyle}`}
           onClick={handleToggle}
         >
           {"Scatter"}
         </button>
         <button
+          data-tag="map"
           className={`w-32 h-full flex items-center justify-center rounded-full bg-transparent  focus:outline-none ${mapButtonStyle}`}
           onClick={handleToggle}
         >
