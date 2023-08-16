24c3b33a62557068b1431cc5855edd1301047c31
RobJ <robjjohnsenwork@gmail.com>
2023-08-16 17:08:19 +0700

    added popup for province page list economic

--- src/components/ProvincePageListComponent.jsx
+++ src/components/ProvincePageListComponent.jsx
@@ -4,6 +4,7 @@ import { getAllProvincesInSelectedCountry } from "@/lib/provinceData";
 import TableRow from "./TableRow";
 import { getProvinceId } from "@/lib/utils";
 import { getAllDistrictsInSelectedProvinceByYear } from "@/lib/districtdata";
+import Link from "next/link";
 
 // getting params and searchParams from the the countryPage which is child of layout... it will rendered when these properties change and thus this should be up to date
 export default async function ProvincePageListComponent({
@@ -48,8 +49,8 @@ export default async function ProvincePageListComponent({
             className="relative group
               "
           >
-            <div className="w-48 tooltip-content hidden group-hover:block absolute z-10 bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full bg-slate-800 text-white text-xs rounded py-1 px-4">
-              <div className="">
+            <div className="w-52 tooltip-content hidden group-hover:block absolute z-10 bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full bg-slate-800 text-white text-xs rounded py-4 px-4">
+              <div className="font-normal">
                 Learn more about how to calculate environmental scores
               </div>
               {/* Link to methodology page in new tab, try include hash key to take user to a section on the page */}
@@ -69,8 +70,40 @@ export default async function ProvincePageListComponent({
             </svg>
           </span>
         </span>
-        <span className="bg-red-300 w-3/12 border-y-2 border-r-2 border-black">
-          Economic
+        <span className="bg-red-300 w-3/12 border-r-2 border-y-2 border-black flex justify-center items-center gap-2 ">
+          <span>Economic</span>
+          <span
+            className="relative group
+              "
+          >
+            <div className="w-52 tooltip-content hidden group-hover:block absolute bottom-0 left-1/2 z-10 transform -translate-x-full translate-y-full bg-slate-800 text-white text-xs rounded py-4 px-4 ">
+              {
+                <div className=" font-normal">
+                  Learn more about how to calculate economic scores
+                </div>
+              }
+              {/* Link to methodology page in new tab, try include hash key to take user to a section on the page */}
+              <Link
+                href="/methodology"
+                target="_blank"
+                className="text-blue-400"
+              >
+                Learn more
+              </Link>
+            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="tooltip w-4 h-4"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 01-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 01-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 01-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584zM12 18a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
         </span>
       </section>
       {/* rows */}
