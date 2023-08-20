158b3a19fe25e2476ed2e3cfed5d593106bde414
RobJ <robjjohnsenwork@gmail.com>
2023-08-20 12:50:47 +0700

    updated release notes

--- src/app/releaseNotes/page.jsx
+++ src/app/releaseNotes/page.jsx
@@ -17,11 +17,42 @@ export default function ReleaseNotes() {
         <div className="w-full h-full border-2 border-slate-400  bg-white overflow-auto unique-scrollbar">
           {/* Inner Box */}
           <div className="flex flex-col p-5 px-7 overflow-auto gap-10 font-inter">
+            {/* UPDATE : v 0.6.2 - MINOR - optimise visuals  */}
+            <div className="flex flex-col  gap-3">
+              <div className="flex flex-col">
+                <span className=" text-slate-400">
+                  Aug 22nd 2023 | Version 0.6.2 beta
+                </span>
+                <span className="font-semibold text-xl">
+                  Optimised Visual component performance
+                </span>
+              </div>
+              <div className=" flex gap-5">
+                <span className="rounded-full bg-[#FFF5CA] text-[#995300] px-4 p-1">
+                  Minor
+                </span>
+                <span
+                  className="rounded-full bg-[#E3FCEF] text-[#044630]
+] px-4 p-1"
+                >
+                  Patch
+                </span>
+              </div>
+              <span className="">
+                Optimized the performance of key visual components by better
+                leveraging the {"library's"} API. Results in reduced client-side
+                latency, and a more fluid and responsive user experience.
+                Moreover, {"we've"} introduced new interactions for both
+                province and district views, granting users the flexibility to
+                toggle between their preferred viewing methods. These
+                enhancements offer a richer visual presentation.
+              </span>
+            </div>
             {/* UPDATE : v 0.6 - MAJOR - new API and DB  */}
             <div className="flex flex-col  gap-3">
               <div className="flex flex-col">
                 <span className=" text-slate-400">
-                  Aug 22th 2023 | Version 0.6 beta
+                  Aug 5th 2023 | Version 0.6 beta
                 </span>
                 <span className="font-semibold text-xl">
                   Enhanced API Layer and Multinational DB Cluster
@@ -50,7 +81,7 @@ export default function ReleaseNotes() {
             <div className="flex flex-col  gap-3">
               <div className="flex flex-col">
                 <span className=" text-slate-400">
-                  July 29th 2023 | Version 0.5 beta
+                  July 19th 2023 | Version 0.5 beta
                 </span>
                 <span className="font-semibold text-xl">
                   Transition to Next.JS and a refined UI Design
