db366571e707d0ccf2aa09bfb5f34da64cd8d636
RobJ <robjjohnsenwork@gmail.com>
2023-08-24 09:41:19 +0700

    removed unused apis

--- src/app/api/districts/route.js
+++ src/app/api/districts/route.js
@@ -1,19 +1,19 @@
-import clientPromise from "@/lib/mongodb";
-import { NextResponse } from "next/server";
+// import clientPromise from "@/lib/mongodb";
+// import { NextResponse } from "next/server";
 
-// lets try get all districts at the api/route
-export async function GET(request) {
-  // console.log("[ENDPOINT HIT] : ", request.url);
-  //
-  const client = await clientPromise;
-  const db = client.db(process.env.MONGO_DB_NAME);
-  const data = await db
-    .collection(`kosovo-province-data`)
-    .find({}, { projection: { _id: 0 } })
-    .toArray();
-  // allDistricts in an array of objects
-  return NextResponse.json(data);
-}
+// // lets try get all districts at the api/route
+// export async function GET(request) {
+//   // console.log("[ENDPOINT HIT] : ", request.url);
+//   //
+//   const client = await clientPromise;
+//   const db = client.db(process.env.MONGO_DB_NAME);
+//   const data = await db
+//     .collection(`kosovo-province-data`)
+//     .find({}, { projection: { _id: 0 } })
+//     .toArray();
+//   // allDistricts in an array of objects
+//   return NextResponse.json(data);
+// }
 
 // export const getDistricts = cache(async (country) => {
 //   const client = await clientPromise;
--- src/app/api/provinces/route.js
+++ src/app/api/provinces/route.js
@@ -1,28 +1,28 @@
-import clientPromise from "@/lib/mongodb";
+// import clientPromise from "@/lib/mongodb";
 
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-// lets try get all districts at the api/route
-export async function GET(request) {
-  const { searchParams } = new URL(request.url);
-  const country = searchParams.get("country");
-  const province = searchParams.get("province");
-  const province_id = searchParams.get("province_id");
+// // lets try get all districts at the api/route
+// export async function GET(request) {
+//   const { searchParams } = new URL(request.url);
+//   const country = searchParams.get("country");
+//   const province = searchParams.get("province");
+//   const province_id = searchParams.get("province_id");
 
-  // console.log(
-  //   "[ENDPOINT HIT] : grabbing district info about a selected province"
-  // );
+//   // console.log(
+//   //   "[ENDPOINT HIT] : grabbing district info about a selected province"
+//   // );
 
-  const client = await clientPromise;
-  const db = client.db(process.env.MONGO_DB_NAME);
+//   const client = await clientPromise;
+//   const db = client.db(process.env.MONGO_DB_NAME);
 
-  const data = await db
-    .collection(`${country}-district-data`)
-    .find({ PROVINCE_ID: province_id }, { _id: 0 })
-    .toArray();
-  //   // allDistricts in an array of objects
-  return NextResponse.json(data);
-}
+//   const data = await db
+//     .collection(`${country}-district-data`)
+//     .find({ PROVINCE_ID: province_id }, { _id: 0 })
+//     .toArray();
+//   //   // allDistricts in an array of objects
+//   return NextResponse.json(data);
+// }
 
 // export const getDistricts = cache(async (country) => {
 //   const client = await clientPromise;
